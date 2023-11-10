44e2975dc5d54e1ee51d6e9b03243e41abe0a35c
Alexandru Fikl <alexfikl@gmail.com>
2023-11-10 19:54:46 +0200

    style: run some formatter

--- background.js
+++ background.js
@@ -1,57 +1,56 @@
-const DEFAULT_UNIVERSITY = "UnivdeVestTM";
+const DEFAULT_UNIVERSITY = 'UnivdeVestTM'
 
-const ENFORMATION_URL = "https://z.e-nformation.ro/$@?action=source&sourceID=$@";
+const ENFORMATION_URL = 'https://z.e-nformation.ro/$@?action=source&sourceID=$@'
 const ENFORMATION_SOURCE_IDS = {
-    "www.sciencedirect.com": "ScienceDirectEbooks_AnelisPlus",
-};
+  'www.sciencedirect.com': 'ScienceDirectEbooks_AnelisPlus'
+}
 
-function transformUrl(url, callback) {
-    var a = new URL(url);
-    if (a.hostname in ENFORMATION_SOURCE_IDS) {
-        browser.storage.sync.get({"university_id": DEFAULT_UNIVERSITY}, (items) => {
-            var university_id = items["university_id"];
-            var eurl = "https://www.e-nformation.ro";
+function transformUrl (url, callback) {
+  let a = new URL(url)
+  if (a.hostname in ENFORMATION_SOURCE_IDS) {
+    browser.storage.sync.get({ university_id: DEFAULT_UNIVERSITY }, (items) => {
+      const university_id = items.university_id
 
-            if (university_id) {
-                eurl = (
-                    ENFORMATION_URL
-                    .replace("$@", university_id)
-                    .replace("$@", ENFORMATION_SOURCE_IDS[a.hostname]));
-            }
-            callback(eurl);
-        });
-    } else {
-        browser.storage.sync.get({"original_url": null}, (items) => {
-            var original_url = items["original_url"];
-            if (original_url) {
-                ao = new URL(original_url);
-                eurl = new URL(ao.pathname, url).href;
+      if (university_id) {
+        const eurl = (
+          ENFORMATION_URL
+            .replace('$@', university_id)
+            .replace('$@', ENFORMATION_SOURCE_IDS[a.hostname]))
+        callback(eurl)
+      }
+    })
+  } else {
+    browser.storage.sync.get({ original_url: null }, (items) => {
+      const original_url = items.original_url
 
-                callback(eurl);
-            }
-        });
-    }
+      if (original_url) {
+        a = new URL(original_url)
+
+        eurl = new URL(a.pathname, url).href
+        callback(eurl)
+      }
+    })
+  }
 }
 
 browser.action.onClicked.addListener((tab) => {
-    transformUrl(tab.url, (newUrl) => {
-        browser.tabs.update(tab.id, {"url": newUrl});
-    });
-});
+  transformUrl(tab.url, (newUrl) => {
+    browser.tabs.update(tab.id, { url: newUrl })
+  })
+})
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
-    transformUrl(info.linkUrl, (newUrl) => {
-        browser.tabs.create({"url": newUrl});
-    });
-});
+  transformUrl(info.linkUrl, (newUrl) => {
+    browser.tabs.create({ url: newUrl })
+  })
+})
 
 browser.runtime.onInstalled.addListener((details) => {
-    browser.storage.sync.get({"university_id": null}, (items) => {
-        browser.contextMenus.create({
-            "title": "Open link through e-nformation",
-            "contexts": ["link"],
-            "id": "redirect"
-        });
-    });
-});
-
+  browser.storage.sync.get({ university_id: null }, (items) => {
+    browser.contextMenus.create({
+      title: 'Open link through e-nformation',
+      contexts: ['link'],
+      id: 'redirect'
+    })
+  })
+})
--- options.js
+++ options.js
@@ -1,67 +1,67 @@
-document.addEventListener("DOMContentLoaded", () => {
-    var status_field;
-    var university_box;
-    var select_box;
-    var select_radio;
-    var custom_radio;
+document.addEventListener('DOMContentLoaded', () => {
+  let statusField
+  let universityBox
+  let selectBox
+  let selectRadio
+  let customRadio
 
-    function init_options() {
-        status_field = document.getElementById("status");
-        university_box = document.getElementById("university_id_box");
-        select_box = document.getElementById("university_id_select");
-        select_radio = document.getElementById("university_select");
-        custom_radio = document.getElementById("university_custom");
-        save_button = document.getElementById("save_button");
+  function initOptions () {
+    statusField = document.getElementById('status')
+    universityBox = document.getElementById('university_id_box')
+    selectBox = document.getElementById('university_id_select')
+    selectRadio = document.getElementById('university_select')
+    customRadio = document.getElementById('university_custom')
+    saveButton = document.getElementById('saveButton')
 
-        select_radio.addEventListener("click", radio_onclick);
-        custom_radio.addEventListener("click", radio_onclick);
-        select_box.addEventListener("change", select_onchange);
-        save_button.addEventListener("click", save_options);
+    selectRadio.addEventListener('click', radioOnClick)
+    customRadio.addEventListener('click', radioOnClick)
+    selectBox.addEventListener('change', selectOnChange)
+    saveButton.addEventListener('click', saveOptions)
 
-        restore_options();
-    }
+    restoreOptions()
+  }
 
-    function save_options() {
-        browser.storage.sync.set({"university_id": university_box.value}, () => {
-            // Update status to let user know options were saved.
-            status_field.innerHTML = "Options Saved.";
-            setTimeout(() => {
-                status_field.innerHTML = "";
-            }, 2000);
-        });
-    }
+  function saveOptions () {
+    browser.storage.sync.set({ university_id: universityBox.value }, () => {
+      // Update status to let user know options were saved.
+      statusField.innerHTML = 'Options Saved.'
+      setTimeout(() => {
+        statusField.innerHTML = ''
+      }, 2000)
+    })
+  }
 
-    function restore_options() {
-        browser.storage.sync.get({"university_id": null}, (items) => {
-            var university_id = items["university_id"];
+  function restoreOptions () {
+    browser.storage.sync.get({ university_id: null }, (items) => {
+      const university_id = items.university_id
 
-            if (university_id) {
-                university_box.value = university_id;
-                select_box.value = university_id;
-                if (select_box.value == university_id) {
-                    select_radio.checked = true;
-                } else {
-                    custom_radio.checked = true;
-                }
-            } else {
-                select_radio.checked = true;
-                select_box.selectedIndex = 0;
-            }
-            radio_onclick();
-            select_onchange();
-        });
-    }
-
-    function select_onchange() {
-        if (select_radio.checked) {
-            university_box.value = select_box.value;
+      if (university_id) {
+        universityBox.value = university_id
+        selectBox.value = university_id
+        if (selectBox.value === university_id) {
+          selectRadio.checked = true
+        } else {
+          customRadio.checked = true
         }
-    }
+      } else {
+        selectRadio.checked = true
+        selectBox.selectedIndex = 0
+      }
+      radioOnClick()
+      selectOnChange()
+    })
+  }
 
-    function radio_onclick() {
-        select_box.disabled = !select_radio.checked;
-        university_box.disabled = select_radio.checked;
+  function selectOnChange () {
+    if (selectRadio.checked) {
+      universityBox.value = selectBox.value
     }
+  }
 
-    init_options();
-});
+  function radioOnClick () {
+    selectBox.disabled = !selectRadio.checked
+    universityBox.disabled = selectRadio.checked
+  }
+
+  initOptions()
+})
