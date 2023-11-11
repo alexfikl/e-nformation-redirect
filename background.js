0a5ca1cf52d2e2e60186ae5dd634cfc301c3c7ea
Alexandru Fikl <alexfikl@gmail.com>
2023-11-11 18:42:11 +0200

    feat: allow different url processing

--- background.js
+++ background.js
@@ -1,70 +1,76 @@
 const DEFAULT_UNIVERSITY = 'UnivdeVestTM'
-
 const ENFORMATION_URL = 'https://z.e-nformation.ro/$@?action=source&sourceID=$@'
+
+function patchGenericURL(url, resource) {
+    var result = new URL(url.href)
+    result.pathname = resource.pathname
+    return result.href
+}
+
 const ENFORMATION_SOURCE_IDS = {
-    // 'oce.ovid.com': 'APAPsychArticles',
-    'pubs.acs.org': 'ACS_AnelisPlus',
-    'pubs.aip.org': 'AIP_AnelisPlus',
-    'journals.aps.org': 'APS_Vest',
-    'www.cabidigitallibrary.org': 'CabiDL_AnelisPlus',
-    'www.ceeol.com': 'CEEOL_journals',
-    // 'www.sciencedirect.com': 'ScienceDirectEbooks_AnelisPlus',
-    'www.sciencedirect.com': 'ScienceDirect_AnelisPlus',
-    // 'www.emerald.com': 'EmeraldJournals_AnelisPlus',
-    // 'www.emerald.com': 'EmeraldEbooks_AnelisPlus',
-    // 'ieeexplore.ieee.org': 'IEEEeBooksNOW_AnelisPlus',
-    'ieeexplore.ieee.org': 'IEEE_IEL_AnelisPlus',
-    'digital-library.theiet.org': 'IETDL_AnelisPlus',
-    // 'access.clarivate.com': 'InCites_UVT',
-    'iopscience.iop.org': 'IOPJournals_AnelisPlus',
-    'mathscinet.ams.org': 'MathSciNet_AnelisPlus',
-    // 'www.nature.com': 'Nature_AnelisPlus',
-    'www.proquest.com': 'PQCentral_AnelisPlus',
-    // 'www.proquest.com': 'PQDT_UVT',
-    'journals.sagepub.com': 'SageJournals_AnelisPlus',
-    'sk.sagepub.com': 'SAGEKnowledgeEbooks_AnelisPlus',
-    'www.scopus.com': 'Scopus_AnelisPlus',
-    'link.springer.com': 'SpringerLink_AnelisPlus',
-    'www.tandfonline.com': 'TandFJournals_UVT',
-    'access.clarivate.com': 'ClarivateWoS_AnelisPlus',
-    // 'onlinelibrary.wiley.com': 'WileyBooks_AnelisPlus',
-    'onlinelibrary.wiley.com': 'WileyJournals_AnelisPlus',
-    'www.degruyter.com': 'DeGruytereBooks_AnelisPlus',
-    'www.igpublish.com': 'iGLibraryALA_AnelisPlus',
-    'portal.igpublish.com': 'iGLibraryALA_AnelisPlus',
+    // 'oce.ovid.com': { name: 'APAPsychArticles', patch: patchGenericURL },
+    'pubs.acs.org': { name: 'ACS_AnelisPlus', patch: patchGenericURL },
+    'pubs.aip.org': { name: 'AIP_AnelisPlus', patch: patchGenericURL },
+    'journals.aps.org': {name: 'APS_Vest', patch: patchGenericURL },
+    'www.cabidigitallibrary.org': { name: 'CabiDL_AnelisPlus', patch: patchGenericURL },
+    'www.ceeol.com': { name: 'CEEOL_journals', patch: patchGenericURL },
+    // 'www.sciencedirect.com': { name: 'ScienceDirectEbooks_AnelisPlus', patch: patchGenericURL },
+    'www.sciencedirect.com': { name: 'ScienceDirect_AnelisPlus', patch: patchGenericURL },
+    'www.emerald.com': { name: 'EmeraldJournals_AnelisPlus', patch: patchGenericURL },
+    // 'www.emerald.com': { name: 'EmeraldEbooks_AnelisPlus', patch: patchGenericURL },
+    // 'ieeexplore.ieee.org': { name: 'IEEEeBooksNOW_AnelisPlus', patch: patchGenericURL },
+    'ieeexplore.ieee.org': { name: 'IEEE_IEL_AnelisPlus', patch: patchGenericURL },
+    'digital-library.theiet.org': { name: 'IETDL_AnelisPlus', patch: patchGenericURL },
+    // 'access.clarivate.com': { name: 'InCites_UVT', patch: patchGenericURL },
+    'iopscience.iop.org': { name: 'IOPJournals_AnelisPlus', patch: patchGenericURL },
+    'mathscinet.ams.org': { name: 'MathSciNet_AnelisPlus', patch: patchGenericURL },
+    // 'www.nature.com': { name: 'Nature_AnelisPlus', patch: patchGenericURL },
+    'www.proquest.com': { name: 'PQCentral_AnelisPlus', patch: patchGenericURL },
+    // 'www.proquest.com': { name: 'PQDT_UVT', patch: patchGenericURL },
+    'journals.sagepub.com': { name: 'SageJournals_AnelisPlus', patch: patchGenericURL },
+    'sk.sagepub.com': { name: 'SAGEKnowledgeEbooks_AnelisPlus', patch: patchGenericURL },
+    'www.scopus.com': { name: 'Scopus_AnelisPlus', patch: patchGenericURL },
+    'link.springer.com': { name: 'SpringerLink_AnelisPlus', patch: patchGenericURL },
+    'www.tandfonline.com': { name: 'TandFJournals_UVT', patch: patchGenericURL },
+    'access.clarivate.com': { name: 'ClarivateWoS_AnelisPlus', patch: patchGenericURL },
+    // 'onlinelibrary.wiley.com': { name: 'WileyBooks_AnelisPlus', patch: patchGenericURL },
+    'onlinelibrary.wiley.com': { name: 'WileyJournals_AnelisPlus', patch: patchGenericURL },
+    'www.degruyter.com': { name: 'DeGruytereBooks_AnelisPlus', patch: patchGenericURL },
+    'www.igpublish.com': { name: 'iGLibraryALA_AnelisPlus', patch: patchGenericURL },
+    'portal.igpublish.com': { name: 'iGLibraryALA_AnelisPlus', patch: patchGenericURL },
 }
 
 function transformUrl (url, callback) {
-    let a = new URL(url)
-    if (a.hostname in ENFORMATION_SOURCE_IDS) {
+    const link_url = new URL(url)
+    if (link_url.hostname in ENFORMATION_SOURCE_IDS) {
         browser.storage.sync.get({ university_id: DEFAULT_UNIVERSITY }, (items) => {
             const university_id = items.university_id
 
             if (university_id) {
+                resource = ENFORMATION_SOURCE_IDS[link_url.hostname]
                 const eurl = (
                     ENFORMATION_URL
                         .replace('$@', university_id)
-                        .replace('$@', ENFORMATION_SOURCE_IDS[a.hostname]))
+                        .replace('$@', resource.name))
                 console.log(eurl)
                 callback(eurl)
             }
         })
-        browser.storage.sync.set({original_url: url})
+        browser.storage.session.set({original_url: url})
     } else {
-        browser.storage.sync.get({ original_url: null }, (items) => {
+        browser.storage.session.get({ original_url: null }, (items) => {
             const original_url = items.original_url
 
             if (original_url) {
-                a = new URL(original_url)
-                eurl = `${url}${a.pathname.slice(1)}`
-                if (a.search) {
-                    eurl = `${eurl}${a.search}`
-                }
+                const resource_url = new URL(original_url)
+                resource = ENFORMATION_SOURCE_IDS[resource_url.hostname]
+
+                eurl = resource.patch(link_url, resource_url)
                 console.log(eurl)
                 callback(eurl)
             }
         })
-        browser.storage.sync.set({original_url: null})
+        browser.storage.session.set({original_url: null})
     }
 }
 
