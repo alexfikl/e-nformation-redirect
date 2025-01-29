c888329f74786d2bbcf1f46b104cc8d45cbf18d5
Alexandru Fikl <alexfikl@gmail.com>
2025-01-29 20:34:22 +0200

    chore: update urls

--- background.js
+++ background.js
@@ -10,7 +10,7 @@ const ENFORMATION_RESOURCES = [
     {
         name: "ACS_AnelisPlus",
         matchPattern: /^https:\/\/pubs\.acs\.org\/(.*?)$/,
-        redirectPattern: "https://0610wbc33-y-https-pubs-acs-org.z.e-nformation.ro/$1",
+        redirectPattern: "https://0610wwvid-y-https-pubs-acs-org.z.e-nformation.ro/$1",
     },
     {
         name: "AIP_AnelisPlus",
@@ -22,84 +22,84 @@ const ENFORMATION_RESOURCES = [
         name: "APS_AnelisPlus",
         matchPattern: /^https:\/\/journals\.aps\.org\/(.*?)$/,
         redirectPattern:
-            "https://06108bc37-y-https-journals-aps-org.z.e-nformation.ro/$1",
+            "https://06111wvih-y-https-journals-aps-org.z.e-nformation.ro/$1",
     },
     {
         name: "CabiDL_AnelisPlus",
         matchPattern: /^https:\/\/www\.cabidigitallibrary\.org\/(.*?)$/,
         redirectPattern:
-            "https://0610hbc38-y-https-www-cabidigitallibrary-org.z.e-nformation.ro/$1",
+            "https://0610hwvik-y-https-www-cabidigitallibrary-org.z.e-nformation.ro/$1",
     },
     {
         name: "CEEOL_journals",
         matchPattern: /^https:\/\/www\.ceeol\.com\/(.*?)$/,
-        redirectPattern: "https://0610abc3a-y-https-www-ceeol-com.z.e-nformation.ro/$1",
+        redirectPattern: "https://06113ww11-y-https-www-ceeol-com.z.e-nformation.ro/$1",
     },
     {
         name: "ScienceDirectEbooks_AnelisPlus",
         matchPattern: /^https:\/\/www\.sciencedirect\.com\/bookseries\/(.*?)$/,
         redirectPattern:
-            "https://0610dg2yd-y-https-www-sciencedirect-com.z.e-nformation.ro/bookseries/$1",
+            "https://0610dww13-y-https-www-sciencedirect-com.z.e-nformation.ro/bookseries/$1",
     },
     {
         name: "ScienceDirect_AnelisPlus",
         matchPattern: /^https:\/\/www\.sciencedirect\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610dbc3e-y-https-www-sciencedirect-com.z.e-nformation.ro/$1",
+            "https://0610dww13-y-https-www-sciencedirect-com.z.e-nformation.ro/$1",
     },
     {
         name: "EmeraldEbooks_AnelisPlus",
         matchPattern: /^https:\/\/www\.emerald\.com\/insight\/publication\/(.*?)$/,
         redirectPattern:
-            "https://06104g2zq-y-https-www-emerald-com.z.e-nformation.ro/insight/publication/$1",
+            "https://0610qww15-y-https-www-emerald-com.z.e-nformation.ro/insight/publication/$1",
     },
     {
         name: "EmeraldJournals_AnelisPlus",
         matchPattern: /^https:\/\/www\.emerald\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610qbc3t-y-https-www-emerald-com.z.e-nformation.ro/$1",
+            "https://06104ww16-y-https-www-emerald-com.z.e-nformation.ro/$1",
     },
     {
         name: "IEEEeBooksNOW_AnelisPlus",
         matchPattern: /^https:\/\/ieeexplore\.ieee\.org\/book\/(.*?)$/,
         redirectPattern:
-            "https://06105g30f-y-https-ieeexplore-ieee-org.z.e-nformation.ro/book/$1",
+            "https://06105ww17-y-https-ieeexplore-ieee-org.z.e-nformation.ro/book/$1",
     },
     {
         name: "IEEE_IEL_AnelisPlus",
         matchPattern: /^https:\/\/ieeexplore\.ieee\.org\/(.*?)$/,
         redirectPattern:
-            "https://06106g30j-y-https-ieeexplore-ieee-org.z.e-nformation.ro/$1",
+            "https://06106ww18-y-https-ieeexplore-ieee-org.z.e-nformation.ro/$1",
     },
     {
         name: "IETDL_AnelisPlus",
         matchPattern: /^https:\/\/digital-library\.theiet\.org\/(.*?)$/,
         redirectPattern:
-            "https://0610ibc3x-y-https-digital--library-theiet-org.z.e-nformation.ro/$1",
+            "https://0610iww19-y-https-digital--library-theiet-org.z.e-nformation.ro/$1",
     },
     {
         name: "InCites_AnelisPlus",
         matchPattern: /^https:\/\/access\.clarivate\.com\/(.*app=incites.*?)$/,
         redirectPattern:
-            "https://0610yg30q-y-https-access-clarivate-com.z.e-nformation.ro/$1",
+            "https://0610yww1c-y-https-access-clarivate-com.z.e-nformation.ro/$1",
     },
     {
         name: "IOPJournals_AnelisPlus",
         matchPattern: /^https:\/\/iopscience\.iop\.org\/(.*?)$/,
         redirectPattern:
-            "https://06109bc46-y-https-iopscience-iop-org.z.e-nformation.ro/$1",
+            "https://06109ww1d-y-https-iopscience-iop-org.z.e-nformation.ro/$1",
     },
     {
         name: "MathSciNet_AnelisPlus",
         matchPattern: /^https:\/\/mathscinet\.ams\.org\/(.*?)$/,
         redirectPattern:
-            "https://0610jbc48-y-https-mathscinet-ams-org.z.e-nformation.ro/$1",
+            "https://0610jww1e-y-https-mathscinet-ams-org.z.e-nformation.ro/$1",
     },
     {
         name: "Nature_AnelisPlus",
         matchPattern: /^https:\/\/www\.nature\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610ubc49-y-https-www-nature-com.z.e-nformation.ro/$1",
+            "https://0610uww1f-y-https-www-nature-com.z.e-nformation.ro/$1",
     },
     {
         name: "PQDT_UVT",
@@ -117,66 +117,66 @@ const ENFORMATION_RESOURCES = [
         name: "SageJournals_AnelisPlus",
         matchPattern: /^https:\/\/journals\.sagepub\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610pbc4f-y-https-journals-sagepub-com.z.e-nformation.ro/$1",
+            "https://0610pww1s-y-https-journals-sagepub-com.z.e-nformation.ro/$1",
     },
     {
         name: "SAGEKnowledgeEbooks_AnelisPlus",
         matchPattern: /^https:\/\/sk\.sagepub\.com\/(.*?)$/,
         redirectPattern:
-            "https://06102bc4h-y-https-sk-sagepub-com.z.e-nformation.ro/$1",
+            "https://06102ww1u-y-https-sk-sagepub-com.z.e-nformation.ro/$1",
     },
     {
         name: "Scopus_AnelisPlus",
         matchPattern: /^https:\/\/www\.scopus\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610ebc4l-y-https-www-scopus-com.z.e-nformation.ro/$1",
+            "https://0610eww1w-y-https-www-scopus-com.z.e-nformation.ro/$1",
     },
     {
         name: "SpringerLink_AnelisPlus",
         matchPattern: /^https:\/\/link\.springer\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610lbc4m-y-https-link-springer-com.z.e-nformation.ro/$1",
+            "https://0610lww1x-y-https-link-springer-com.z.e-nformation.ro/$1",
     },
     {
         name: "Statista_UVT",
         matchPattern: /^https:\/\/www\.statista\.com\/(.*?)$/,
-        redirectPattern: "https://0610zwux1-y-https-www-statista-com.z.e-nformation.ro/$1",
+        redirectPattern: "https://0610zww1y-y-https-www-statista-com.z.e-nformation.ro/$1",
     },
     {
         name: "TandFJournals_AnelisPlus",
         matchPattern: /^https:\/\/www\.tandfonline\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610obc4n-y-https-www-tandfonline-com.z.e-nformation.ro/$1",
+            "https://06110ww21-y-https-www-tandfonline-com.z.e-nformation.ro/$1",
     },
     {
         name: "ClarivateWoS_AnelisPlus",
         matchPattern: /^https:\/\/access\.clarivate\.com\/(.*app=wos.*?)$/,
         redirectPattern:
-            "https://0610mbc4o-y-https-www-webofscience-com.z.e-nformation.ro/$1",
+            "https://0610mww24-y-https-www-webofscience-com.z.e-nformation.ro/$1",
     },
     {
         name: "WileyBooks_AnelisPlus",
         matchPattern: /^https:\/\/onlinelibrary\.wiley\.com\/doi\/book\/(.*?)$/,
         redirectPattern:
-            "https://0610gg33b-y-https-onlinelibrary-wiley-com.z.e-nformation.ro/doi/book/$1",
+            "https://0610gww25-y-https-onlinelibrary-wiley-com.z.e-nformation.ro/doi/book/$1",
     },
     {
         name: "WileyJournals_AnelisPlus",
         matchPattern: /^https:\/\/.*onlinelibrary\.wiley\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610fbc4s-y-https-onlinelibrary-wiley-com.z.e-nformation.ro/$1",
+            "https://0610fww26-y-https-onlinelibrary-wiley-com.z.e-nformation.ro/$1",
     },
     {
         name: "DeGruytereBooks_AnelisPlus",
         matchPattern: /^https:\/\/www\.degruyter\.com\/(.*?)$/,
         redirectPattern:
-            "https://06103bc4t-y-https-www-degruyter-com.z.e-nformation.ro/$1",
+            "https://06103ww2b-y-https-www-degruyter-com.z.e-nformation.ro/$1",
     },
     {
         name: "iGLibraryALA_AnelisPlus",
         matchPattern: /^https:\/\/.*\.igpublish\.com\/(.*?)$/,
         redirectPattern:
-            "https://0610nbc4u-y-https-portal-igpublish-com.z.e-nformation.ro/$1",
+            "https://0610nww2c-y-https-portal-igpublish-com.z.e-nformation.ro/iglibrary/$1",
     },
 ];
 
